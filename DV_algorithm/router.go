7ab6542a5350ac14790988c96918d1d3821d3d8a
yesmola <1062349166@qq.com>
2020-12-08 16:51:20 +0800

    transmit packet

--- DV_algorithm/router.go
+++ DV_algorithm/router.go
@@ -1,40 +1,50 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
 const Unreachable = 16
 
 type TableValue struct {
-	Dist  int
+	Dist int
 	Route []string
 }
-
 type Router struct {
-	Id          string "Router's id"
-	Port        int    "Router's port"
-	Neighbour   []int  "neighbour's port"
-	RouterTable map[int]TableValue
+	Id           string "Router's id"
+	Port         int    "Router's port"
+	Neighbour    []int  "neighbour's port"
+	RoutingTable map[string]TableValue
 }
 
+/*
+ * print router's id and port and neighbour's port
+ */
 func (r *Router) print() {
-	fmt.Println("Id: ",r.Id)
-	fmt.Println("Port: ", r.Port)
-	fmt.Print("Neighbours: ")
-	for _,v := range r.Neighbour {
-		fmt.Print(v," ")
+	fmt.Println("Router Information:")
+	fmt.Println("Id         ", r.Id)
+	fmt.Println("Port       ", r.Port)
+	fmt.Print("Neighbours  ")
+	for _, v := range r.Neighbour {
+		fmt.Print(v, " ")
 	}
 	fmt.Print("\n")
 }
 
+/*
+ * run a udp server to listen the neighbour's router table.
+ */
 func (r *Router) server() {
-	p := make([]byte, 2048)
+	p := make([]byte, 4096)
 	addr := net.UDPAddr{
 		Port: r.Port,
 		IP:   net.ParseIP("127.0.0.1"),
@@ -44,53 +54,184 @@ func (r *Router) server() {
 		log.Println("Some error when start a UDP server: ", err)
 		return
 	}
+
 	for {
 		_, remoteAddr, err := ser.ReadFromUDP(p)
-		fmt.Printf("Read a message from %v %s \n", remoteAddr, p)
 		if err != nil {
-			fmt.Printf("Some error  %v", err)
+			fmt.Println("Some error", err)
 			continue
+		}
+
+		var Message Router
+		buffer := bytes.NewBuffer(p)
+		decoder := gob.NewDecoder(buffer)
+		err = decoder.Decode(&Message)
+		if err != nil {
+			fmt.Println("Some error", err)
+			continue
+		}
+		log.Println("Read a message from", remoteAddr.Port, Message)
+
+		// update routing table
+		remotePort := remoteAddr.Port - 1000
+		remoteId := string(p)
+		_, ok := r.RoutingTable[remoteId]
+		// if that router is not in routing table
+		if ok == false && r.isNeighbour(remotePort) {
+			r.RoutingTable[remoteId] = TableValue{
+				Dist: 1,
+				Route: []string{remoteId},
+			}
 		}
 		//go sendResponse(ser, remoteAddr)
 	}
 }
 
-func (r *Router) broadcast(port int) {
-	//p := make([]byte, 2048)
-	conn, err := net.Dial("udp", "127.0.0.1:"+strconv.Itoa(port))
-	if err != nil {
-		fmt.Printf("Some error %v", err)
-		return
+/*
+ * broadcast current router table to neighbours
+ */
+func (r *Router) broadcast() {
+	for {
+		for i := 0; i < len(r.Neighbour); i++ {
+			port := r.Neighbour[i]
+			lAddr, _ := net.ResolveUDPAddr("udp", "127.0.0.1:"+strconv.Itoa(r.Port+1000))
+			rAddr, _ := net.ResolveUDPAddr("udp", "127.0.0.1:"+strconv.Itoa(port))
+			conn, err := net.DialUDP("udp", lAddr, rAddr)
+
+			if err != nil {
+				fmt.Println("Some error", err)
+				return
+			}
+
+			var buffer bytes.Buffer
+			encoder := gob.NewEncoder(&buffer)
+			err = encoder.Encode(r)
+			if err != nil {
+				fmt.Println("Some error", err)
+				return
+			}
+
+			_,err = conn.Write(buffer.Bytes())
+			if err != nil {
+				fmt.Println("Some error", err)
+				return
+			}
+
+			_ = conn.Close()
+			buffer.Reset()
+		}
+		time.Sleep(3 * time.Second)
 	}
-	_,_ = fmt.Fprintf(conn, "Hi UDP Server, How are you doing?")
 }
 
+/*
+ * check whether the router is neighbour or not
+ * return true or false
+ */
+func (r *Router) isNeighbour(port int) bool {
+	for _, v := range r.Neighbour {
+		if v == port {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+ * print all neighbours who are active
+ */
+func (r *Router) listNBsAlive() {
+	flag := 0
+	for k, v := range r.RoutingTable {
+		if v.Dist < Unreachable {
+			flag = 1
+			fmt.Print(k, " ")
+		}
+	}
+	if flag == 0 {
+		fmt.Print("Empty")
+	}
+	fmt.Print("\n")
+}
+
+/*
+ * print current routing table
+ */
+func (r *Router) printRoutingTable() {
+	fmt.Println("Destination Route")
+	for k,v := range r.RoutingTable {
+		fmt.Print("     ",k,"        ",v.Route[0])
+		for i:=1;i<len(v.Route);i++ {
+			fmt.Print(","+v.Route[i])
+		}
+		fmt.Println()
+	}
+}
 
 func main() {
 	argNum := len(os.Args)
-	if argNum <= 1 {
+	if argNum == 1 {
+		fmt.Println("Usage: go run router.go [router's id],[router's port],[neighbour's port]")
+	}
+	info := strings.Split(os.Args[1], ",")
+	if len(info) <= 2 {
 		log.Println("That a isolated router!")
 		return
 	}
 	var r Router
-	// initialization
-	r.Id = os.Args[1]
-	r.Port, _ = strconv.Atoi(os.Args[2])
-	// r.Neighbour = make([]int, argNum-3)
-	r.RouterTable = make(map[int]TableValue, argNum-3)
-	for i := 3; i < argNum; i++ {
-		neighbourPort, _ := strconv.Atoi(os.Args[i])
+	// initialization and make all neighbours unreachable until receive their heartbeat
+	r.Id = info[0]
+	r.Port, _ = strconv.Atoi(info[1])
+	for i := 2; i < len(info); i++ {
+		neighbourPort,_ := strconv.Atoi(info[i])
 		r.Neighbour = append(r.Neighbour, neighbourPort)
-		r.RouterTable[neighbourPort] = TableValue{Unreachable, []string{}}
+	}
+	r.RoutingTable = make(map[string]TableValue,10)
+	r.RoutingTable[r.Id] = TableValue{
+		Dist: 0,
+		Route: []string{},
 	}
 	// print router state
 	r.print()
+
 	go r.server()
+	go r.broadcast()
+
+	// get stdio from user
 	for {
-		for i:=0;i<len(r.Neighbour);i++ {
-			go r.broadcast(r.Neighbour[i])
+		buf := bufio.NewReader(os.Stdin)
+		fmt.Print("> ")
+		sentence, err := buf.ReadString('\n')
+		// move the last '\n'
+		sentence = strings.Replace(sentence, "\n", "", -1)
+		command := strings.Split(sentence, " ")
+		if err != nil {
+			panic(err)
 		}
-		time.Sleep(3 * time.Second)
+
+		// run the command
+		switch command[0] {
+		case "N":
+			/*
+			 * Print activity’s adjacent list.
+			 * Format: 4 5 (in one line, space separated)
+			 * If having no neighbors，then print “Empty”
+			 */
+			r.listNBsAlive()
+		case "RT":
+			/*
+			 * Export the routes that reach to every destination node.
+			 * Each route occupies one line.
+			 * Format:
+			 * Destination route
+			 * 1 4,3
+			 * 4 5,4
+			 */
+			 r.printRoutingTable()
+		default:
+			fmt.Println("Unknown command")
+		}
 	}
+
 	return
 }
